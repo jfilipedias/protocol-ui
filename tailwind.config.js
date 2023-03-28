2845a41aa24d73a8c055725807883ce95dc33710
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-03-28 15:01:14 -0300

    feat: Add tailwind dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+}
